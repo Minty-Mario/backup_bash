bacc2f6e653b72803ae2b726b71655bddfcd0ed7
Minty-Mario <139778674+Minty-Mario@users.noreply.github.com>
2023-08-08 16:00:12 +0100

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,28 @@
+#Making Variables
+NOW=$(date +"%M_%d_%m_%Y");
+FTP_HOST="HOST"
+USERNAME="USERNAME"
+PASSWORD="PASSWORD"
+FILE="Backup_$NOW.tar.gz"
+DBNAME="DBNAME"
+
+#Dumping database in new Dir
+mkdir Backup_$NOW
+cd Backup_$NOW
+/usr/bin/mysqldump $DBNAME > Backup_$NOW.sql
+
+#Creating tar ball
+tar -czf Backup_$NOW.tar.gz Backup_$NOW.sql
+
+#FTP Transfer
+ftp -p -n $FTP_HOST <<EXIT_SCRIPT
+user $USERNAME $PASSWORD
+binary
+put $FILE /$FILE
+quit
+EXIT_SCRIPT
+
+#Cleaning up ...
+rm Backup_$NOW.sql
+cd ..
+rm -rf Backup_$NOW
